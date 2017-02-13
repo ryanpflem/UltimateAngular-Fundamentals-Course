4b136955c068f24324e0a69095cf27059f4a1a60
Ryan Fleming <fleming.rp@gmail.com>
2017-02-13 16:10:33 -0500

    Getting started

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,15 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      {{title}}
     </div>
   `
 })
 export class AppComponent {
+  title: string;
+
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
+    BrowserModule,
+    CommonModule
   ],
   declarations: [
     AppComponent
+  ],
+  bootstrap: [
+    AppComponent
   ]
 })
-export class AppModule {}
+export class AppModule { }
