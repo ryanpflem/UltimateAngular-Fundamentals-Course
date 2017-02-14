632e5b284f824de1aaf59a16ba7d1ec948057d10
Ryan Fleming <fleming.rp@gmail.com>
2017-02-14 11:45:51 -0500

    ngStyle and style bindings

--- app/app.component.ts
+++ app/app.component.ts
@@ -29,6 +29,24 @@ interface Passenger {
           {{i}}: {{passenger.fullName}}
         </li>
       </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span 
+          class="status"
+          [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71' : '#c0392b')"></span>
+          {{i}}: {{passenger.fullName}}
+        </li>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span 
+          class="status"
+          [ngStyle]="{ backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b') }"></span>
+          {{i}}: {{passenger.fullName}}
+        </li>
+      </ul>
     </div>
   `
 })
