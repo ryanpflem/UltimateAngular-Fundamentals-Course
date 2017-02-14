8f9ef666ebe6b5acabffaf94e1e5de1576a4bb5e
Ryan Fleming <fleming.rp@gmail.com>
2017-02-14 12:35:50 -0500

    Creating a container (smart) component

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    //Custom Modules
+    PassengerDashboardModule
   ],
   declarations: [
     AppComponent
