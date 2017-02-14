9844381a9706e80f6fd061b2fbb424c793c75add
Ryan Fleming <fleming.rp@gmail.com>
2017-02-14 11:55:33 -0500

    Pipes for data transformation

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 interface Passenger {
-    id: number,
-    fullName: string,
-    checkedIn: boolean
-  }
+  id: number,
+  fullname: string,
+  checkedIn: boolean,
+  checkInDate?: number | null
+}
 
 @Component({
   selector: 'app-root',
@@ -15,69 +16,44 @@ interface Passenger {
       <ul>
         <li *ngFor="let passenger of passengers; let i = index;">
           <span 
-          class="status"
-          [class.checked-in]="passenger.checkedIn"></span>
-          {{i}}: {{passenger.fullName}}
-        </li>
-      </ul>
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span 
-          class="status"
-          [ngClass]="{ 'checked-in': passenger.checkedIn }"></span>
-          {{i}}: {{passenger.fullName}}
-        </li>
-      </ul>
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span 
-          class="status"
-          [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71' : '#c0392b')"></span>
-          {{i}}: {{passenger.fullName}}
-        </li>
-      </ul>
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span 
-          class="status"
-          [ngStyle]="{ backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b') }"></span>
-          {{i}}: {{passenger.fullName}}
+            class="status"
+            [class.checked-in]="passenger.checkedIn"></span>
+          {{ i }}: {{ passenger.fullname }}
+          <p>{{ passenger | json }}</p>
+          <div class="date">
+            Check in date: 
+            {{ passenger.checkInDate ? (passenger.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
+          </div>
         </li>
       </ul>
     </div>
   `
 })
 export class AppComponent {
-
-  passengers: Passenger[] = [
-    {
-      id: 1,
-      fullName: 'Stephen',
-      checkedIn: true,
-    },
-    {
-      id: 2,
-      fullName: 'Rose',
-      checkedIn: false,
-    },
-    {
-      id: 3,
-      fullName: 'James',
-      checkedIn: true,
-    },
-    {
-      id: 4,
-      fullName: 'Louise',
-      checkedIn: true,
-    },
-    {
-      id: 5,
-      fullName: 'Tina',
-      checkedIn: false,
-    },
-  ]
-
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true,
+    checkInDate: 1490742000000
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false,
+    checkInDate: null
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true,
+    checkInDate: 1491606000000
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true,
+    checkInDate: 1488412800000
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false,
+    checkInDate: null
+  }];
 }
