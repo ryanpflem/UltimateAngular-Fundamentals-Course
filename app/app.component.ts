fac52bc4a9407a58b75352c18818eb0995669d95
Ryan Fleming <fleming.rp@gmail.com>
2017-02-15 09:58:28 -0500

    Styling active routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
+
+      <nav class="nav">
+        <a routerLink="/" 
+          routerLinkActive="active" 
+          [routerLinkActiveOptions]="{ exact: true}">
+          Home
+        </a>
+        <a routerLink="/oops" 
+          routerLinkActive="active">
+          404
+        </a>    
+      </nav>
+
       <router-outlet></router-outlet>
     </div>
   `
