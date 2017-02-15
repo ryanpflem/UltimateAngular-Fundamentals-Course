7ae394de12ae5b8939a8a676bca0c51faad7c8bb
Ryan Fleming <fleming.rp@gmail.com>
2017-02-15 09:07:01 -0500

    Root module routes and outlet

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,32 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  }
+]
 
 @NgModule({
   imports: [
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
     //Custom Modules
     PassengerDashboardModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   bootstrap: [
     AppComponent
